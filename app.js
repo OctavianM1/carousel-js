c2dfd0a3bbba80a55ab5d5d571f3e3ccf21bfb81
Octavian Mitu <octavian.mitu2000@gmail.com>
2022-05-26 14:48:51 +0300

    added transition

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const container = document.getElementById("container");
 const imgCount = container.children.length - 2;
 const IMG_WIDTH = 400;
-container.style.left = `${-IMG_WIDTH}px`;
+setPxValue(-IMG_WIDTH);
+
+let prevImgLeftPx, curImgLeftPx, nextImgLeftPx;
+setFirstImgLeftPx();
 
 let isPointerDown = false;
 let initMouseX = null;
 let initContainerLeft = -IMG_WIDTH;
+let isActiveTransition = false;
 
 container.addEventListener("mousedown", onMoveStart);
 container.addEventListener("touchstart", onMoveStart);
@@ -17,24 +21,74 @@ window.addEventListener("mousemove", onMove);
 window.addEventListener("touchmove", onMove);
 
 function onMoveStart(e) {
-  initMouseX = getClientX(e);
-  isPointerDown = true;
+  if (!isActiveTransition) {
+    initMouseX = getClientX(e);
+    isPointerDown = true;
+  }
 }
 
 function onMoveEnd() {
   initMouseX = null;
-  isPointerDown = false;
-  initContainerLeft = getPxValue(container.style.left);
+  initContainerLeft = getPxValue();
+
+  if (isPointerDown) {
+    isPointerDown = false;
+    if (getPxValue() !== curImgLeftPx) {
+      container.style.transition = "200ms";
+      container.addEventListener("transitionend", removeTransition);
+      isActiveTransition = true;
+    }
+    if (shouldMoveNext()) {
+      moveTo(nextImgLeftPx);
+      calculateNextImgPx();
+    } else if (shouldMovePrev()) {
+      moveTo(prevImgLeftPx)
+      calculatePrevImgPx();
+    } else {
+      moveTo(curImgLeftPx);
+    }
+  }
+
+  function removeTransition() {
+    container.removeEventListener("transitionend", removeTransition);
+    container.style.transition = "none";
+    isActiveTransition = false;
+    if (getPxValue() === -IMG_WIDTH * (imgCount + 1)) {
+      setPxValue(-IMG_WIDTH);
+      initContainerLeft = -IMG_WIDTH;
+    } else if (getPxValue() === 0) {
+      setPxValue(-IMG_WIDTH * imgCount);
+      initContainerLeft = -IMG_WIDTH * imgCount;
+    }
+  }
+
+  function calculatePrevImgPx() {
+    if (curImgLeftPx === -IMG_WIDTH) {
+      setLastImgLeftPx();
+    } else {
+      nextImgLeftPx = curImgLeftPx;
+      curImgLeftPx = prevImgLeftPx;
+      prevImgLeftPx += IMG_WIDTH;
+    }
+  }
+
+  function calculateNextImgPx() {
+    if (nextImgLeftPx === -IMG_WIDTH * (imgCount + 1)) {
+      setFirstImgLeftPx();
+    } else {
+      prevImgLeftPx = curImgLeftPx;
+      curImgLeftPx = nextImgLeftPx;
+      nextImgLeftPx -= IMG_WIDTH;
+    }
+  }
 }
 
 function onMove(e) {
   if (isPointerDown) {
-    container.style.left = `${getDx(e) + initContainerLeft}px`;
-    if (getPxValue(container.style.left) <= -1 * IMG_WIDTH * (imgCount + 1)) {
-      container.style.left = `-${IMG_WIDTH}px`;
+    setPxValue(getDx(e) + initContainerLeft);
+    if (getPxValue() <= -1 * IMG_WIDTH * (imgCount + 1)) {
       initContainerLeft = -IMG_WIDTH - getDx(e);
-    } else if (getPxValue(container.style.left) >= 0) {
-      container.style.left = `-${imgCount * IMG_WIDTH}px`;
+    } else if (getPxValue() >= 0) {
       initContainerLeft = -1 * imgCount * IMG_WIDTH - getDx(e);
     }
   }
@@ -51,6 +105,35 @@ function getClientX(e) {
   return e.touches[0].clientX;
 }
 
-function getPxValue(val) {
-  return parseInt(val.split("px")[0]);
+function getPxValue() {
+  return parseInt(container.style.transform.split("(")[1].split("px")[0]);
+}
+
+function setPxValue(val) {
+  container.style.transform = `translateX(${val}px)`;
+}
+
+function setFirstImgLeftPx() {
+  prevImgLeftPx = 0;
+  curImgLeftPx = -IMG_WIDTH;
+  nextImgLeftPx = -IMG_WIDTH * 2;
+}
+
+function setLastImgLeftPx() {
+  prevImgLeftPx = -IMG_WIDTH * (imgCount - 1);
+  curImgLeftPx = -IMG_WIDTH * imgCount;
+  nextImgLeftPx = -IMG_WIDTH * (imgCount + 1);
+}
+
+function shouldMoveNext() {
+  return getPxValue() < curImgLeftPx - IMG_WIDTH * 0.1;
+}
+
+function shouldMovePrev() {
+  return getPxValue() > curImgLeftPx + IMG_WIDTH * 0.1;
+}
+
+function moveTo(val) {
+  setPxValue(val);
+  initContainerLeft = val;
 }
